2f2434f4c2170a14eaeeac92b359a4ca8dab6665
ascode94 <ascode94@gmail.com>
2020-07-11 20:07:14 +0430

    implemented firebase utils included ability to store authenticated user into our firebase database

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
     constructor() {
@@ -22,21 +22,34 @@ class App extends React.Component {
     unsubscribeFromAuth = null
 
     componentDidMount() {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({ currentUser: user })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth)
 
-        console.log(user)
-      })
+                userRef.onSnapshot(snapShpt => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShpt.id,
+                            ...snapShpt.data()
+                        }
+                    })
+
+                    console.log(this.state)
+                })
+            } else {
+                this.setState({currentUser: userAuth})
+            }
+        })
     }
 
     componentWillUnmount() {
-      this.unsubscribeFromAuth()
+        this.unsubscribeFromAuth()
     }
 
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
