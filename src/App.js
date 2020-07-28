d722ef06f95802a72d2d732a2d2d1e52bfa46d37
ascode94 <ascode94@gmail.com>
2020-07-28 12:05:26 +0430

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckOutPage from './pages/checkout/checkout.component'
 
 import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import {setCurrentUser} from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 class App extends React.Component {
     unsubscribeFromAuth = null
@@ -21,6 +24,7 @@ class App extends React.Component {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth)
+                await console.log(userAuth)
                 userRef.onSnapshot(snapShpt => {
                     setCurrentUser({
                         id: snapShpt.id,
@@ -45,6 +49,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckOutPage}/>
                     <Route
                         exact
                         path='/signin'
@@ -57,7 +62,9 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({user}) => ({currentUser: user.currentUser})
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+})
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
