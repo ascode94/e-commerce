c82e216238cd778b0d580b847e64d364f0b3c8c8
ascode94 <ascode94@gmail.com>
2020-09-10 19:27:44 +0430

    Before Refactor Email Sagas

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -75,10 +75,10 @@ export const convertCollectionsSnapshotToMap = (collections) => {
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
-const provider = new firebase
+export const googleProvider = new firebase
     .auth
     .GoogleAuthProvider()
-provider.setCustomParameters({prompt: 'select_account'})
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+    googleProvider.setCustomParameters({prompt: 'select_account'})
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
 export default firebase
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -12,3 +12,12 @@ export const selectCollectionsForPreview = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector([selectCollections], collections => (collections ? collections[collectionUrlParam] : null))
 
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
+
+export const selectIsCollectionLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+)
